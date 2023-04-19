b8e33fd3721473a6bda0d3a581fc4985d29054a4
JTxCo <103444631+JTxCo@users.noreply.github.com>
2023-04-19 12:13:36 -0600

    ClientServerConnection
    connected the client and server, basic info sent

--- src/Client.java
+++ src/Client.java
@@ -1,3 +1,11 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.nio.Buffer;
+
 public class Client {
     ClientController clientController;
     public static void main(String[] args) {
@@ -7,5 +15,28 @@ public class Client {
     }
     void createController(){
         clientController = new ClientController();
+
+
+        //client Connecting to server
+        try {
+            Socket socket = new Socket("localhost", 5001);
+            BufferedReader socketReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            BufferedReader inputReader = new BufferedReader(new InputStreamReader(System.in));
+            PrintWriter socketWriter = new PrintWriter(socket.getOutputStream());
+            String line = "testing";
+            while((line = inputReader.readLine())!=null){
+                socketWriter.println(line);
+                socketWriter.flush();
+                String retval = socketReader.readLine();
+                System.out.println(retval);
+            }
+        } catch (UnknownHostException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
     };
 }
--- src/Server.java
+++ src/Server.java
@@ -1,3 +1,10 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
     GameLogic gameLogic;
     DoQueries doQueries;
@@ -7,5 +14,24 @@ public class Server {
     public void createServer() {
         gameLogic = new GameLogic();
         doQueries = new DoQueries();
+
+
+        //start the server
+        try {
+            ServerSocket serverSocket = new ServerSocket(5001);
+            Socket conn = serverSocket.accept();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            PrintWriter printWriter = new PrintWriter(conn.getOutputStream());
+            String line;
+            while((line = bufferedReader.readLine()) != null ){
+                System.out.println("Server received: " + line);
+                printWriter.println(line);
+                printWriter.flush();
+            }
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
     }
 }
