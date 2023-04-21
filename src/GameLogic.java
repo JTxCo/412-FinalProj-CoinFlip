90e7bf1233d4255d1051e78f8b7886941052f692
allisonjbeasley <allisonjbeasley@gmail.com>
2023-04-21 11:46:34 -0600

    Fixing rand num

--- src/GameLogic.java
+++ src/GameLogic.java
@@ -11,7 +11,8 @@ public class GameLogic {
     //create a function that flips a coin and returns the result
     //not returning anything at the momment
     public String FlipCoin(){
-        rand = (int)(Math.random())*10;
+        double num = Math.random()*10;
+        rand = (int)num;
         rand = rand%2;
         if (rand==0){
             result = "Heads";
