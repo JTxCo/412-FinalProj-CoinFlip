6d01ded011ed6bc14a7d46f99662d81566379936
allisonjbeasley <allisonjbeasley@gmail.com>
2023-04-21 11:25:09 -0600

    game log merge

--- src/GameLogic.java
+++ src/GameLogic.java
@@ -10,8 +10,8 @@ public class GameLogic {
     }
     //create a function that flips a coin and returns the result
     //not returning anything at the momment
-    public void FlipCoin(){
-        rand = (int)(Math.random())*10;
+    public String FlipCoin(){
+        rand = (int)((Math.random())*10);
         rand = rand%2;
         if (rand==0){
             result = "Heads";
@@ -19,6 +19,7 @@ public class GameLogic {
         else {
             result = "Tails";
         }
+        return result;
 
     }
     public void DisplayResult(){
