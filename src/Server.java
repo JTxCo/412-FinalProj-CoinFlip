00c8ddc18f3ed66b71e26dbf4644b8e582dd8bf4
JTxCo <103444631+JTxCo@users.noreply.github.com>
2023-04-26 11:44:18 -0600

    added firstMultithread attempt
    is not fully working that i know of, will work on later

--- src/Server.java
+++ src/Server.java
@@ -8,30 +8,71 @@ import java.net.Socket;
 public class Server {
     GameLogic gameLogic;
     DoQueries doQueries;
+    ServerSocket serverSocket;
     public static void main(String[] args) {
         new Server().createServer();
     }
     public void createServer() {
         gameLogic = new GameLogic();
         doQueries = new DoQueries();
-
-
-        //start the server
         try {
-            ServerSocket serverSocket = new ServerSocket(5001);
-            Socket conn = serverSocket.accept();
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-            PrintWriter printWriter = new PrintWriter(conn.getOutputStream());
-            String line;
-            while((line = bufferedReader.readLine()) != null ){
-                System.out.println("Server received: " + line);
-                printWriter.println(gameLogic.FlipCoin());
-                printWriter.flush();
-            }
+            serverSocket = new ServerSocket(5001);
         } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
+        //start the server
+        try {
+            serverSocket.setReuseAddress(true);
+            while(true){
+                Socket conn = serverSocket.accept();
+                System.out.println("Connection established");
+                ClientConnection clientConnection = new ClientConnection(conn);
+                new Thread(clientConnection).start();
+            }
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }finally{
+            if(serverSocket != null)
+                try {
+                    serverSocket.close();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+        }
 
     }
+    private class ClientConnection implements Runnable{
+        private Socket clientSocket;
+        BufferedReader bufferedReader;
+        PrintWriter printWriter;    
+       public  ClientConnection(Socket clientSocket){
+            this.clientSocket = clientSocket;
+        }
+        @Override
+        public void run(){
+            String line;
+            try {
+                bufferedReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                printWriter = new PrintWriter(clientSocket.getOutputStream());
+                while((line = bufferedReader.readLine()) != null ){
+                    System.out.println("Server received: " + line);
+                    printWriter.println(gameLogic.FlipCoin());
+                    printWriter.flush();
+                }
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            }
+    }
+
 }
+
+
+
+
+
+
