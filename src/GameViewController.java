d262d14e12605a4bebaa65f43fdb26726757bcea
allisonjbeasley <allisonjbeasley@gmail.com>
2023-05-01 20:01:47 -0600

    Started GameView GUI

--- src/GameViewController.java
+++ src/GameViewController.java
@@ -1,10 +1,29 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class GameViewController {
     private GameView gameView;
     public GameViewController() {
         // Create a new instance of the class
         // and call the method
         gameView = new GameView();
+        gameView.setPlayButtonActionListener(new PlayButtonActionListener());
 
+    }
+
+    public class PlayButtonActionListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if (gameView.getHeadsButton().isSelected()){
+                System.out.println("Heads button selected!");
+            }
+            else if(gameView.getTailsButton().isSelected()){
+                System.out.println("Tails button selected!");
+            }
+            else {
+                System.out.println("Please select a button");
+            }
+        }
     }
 
     public String getPlayInput() {
