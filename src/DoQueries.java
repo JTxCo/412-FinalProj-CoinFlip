f6d3378143a715293964c93a41220abb42399bae
JTxCo <103444631+JTxCo@users.noreply.github.com>
2023-05-01 18:09:25 -0600

    did basic JDBC myself adding

--- src/DoQueries.java
+++ src/DoQueries.java
@@ -1,11 +1,28 @@
 import javax.swing.*;
-
+import java.sql.*;
 public class DoQueries {
     public DoQueries() {
-        // Create a new instance of the class
+           // Create a new instance of the class
         // and call the method
+        String url = "jdbc:mysql://localhost:3306/coinflip";
+        staString DB_DRV = "com.mysql.jdbc.Driver";
+        String username = "root";
+        String password = "root";
+        try {
+            Connection conn = DriverManager.getConnection(url, username, password);
+            Statement stmt = conn.createStatement();
+            String createDB = "CREATE TABLE IF NOT EXISTS users " + "(username VARCHAR(50), " + "password VARCHAR(50), " + "balance INT(255), " + "PRIMARY KEY ( username ))";
+            stmt.executeUpdate(createDB);
+        System.out.println("created db");
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
-    public void UpdateBalance(){}; // Update the balance of the user
+    public void UpdateBalance(){
+        
+
+    }; // Update the balance of the user
     public void InitializeUser(){}; // Initialize the user
     public void verifyUser(){}; // Verify the user
     public Boolean isUsernameTaken(){
