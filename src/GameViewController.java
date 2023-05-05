550ce41f1b0a780107a48e118014c4802b51010d
allisonjbeasley <allisonjbeasley@gmail.com>
2023-05-04 19:00:09 -0600

    Tails button update

--- src/GameViewController.java
+++ src/GameViewController.java
@@ -25,6 +25,7 @@ public class GameViewController {
         socket = s;
         gameView = new GameView();
         gameView.setPlayButtonActionListener(new PlayButtonActionListener());
+        //gameView.setBetButtonActionListener(new BetButtonActionListener());
 
     }
 
@@ -54,10 +55,33 @@ public class GameViewController {
             else if(gameView.getTailsButton().isSelected()){
                 System.out.println("Tails button selected!");
                 playInput="Tails";
+                try {
+                    BufferedReader socketReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    PrintWriter socketWriter = new PrintWriter(socket.getOutputStream());
+                    System.out.println("client> sent to server: "+playInput);
+                    socketWriter.println(playInput);
+                    socketWriter.flush();
+                    String retval = socketReader.readLine();
+                    System.out.println("client> recieved result from server coin flip: "+retval);
+                    String betResult = socketReader.readLine();
+                    System.out.println("client> bet result: "+betResult);
+
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
             }
             else {
                 System.out.println("Please select a button");
             }
+        }
+    }
+
+    public class BetButtonActionListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            Double betInput = Double.parseDouble(gameView.getBetTextField().getText());
+            System.out.println("Bet amount entered: "+betInput);
+
         }
     }
 
